9551ab6309388c553cfad144e73a8930029ae223
Sire-eben <ajewoleebenezer99@gmail.com>
2024-02-03 10:48:39 +0100

    init

---
+++ lib/return_policy.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+class ReturnPolicy extends StatelessWidget {
+  const ReturnPolicy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Text(
+              "CAPFIELDS EVOLVE RETURN POLICY",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+                '''Refund policy brings about refund timeline. If we deliver an incorrect or damaged item (damage that has been caused by us), bought from only 9 STORE OR PHARMACY) we will happily replace the item at no additional cost to you. For us to replace such items, customers will need to notify the delivery driver of their observation at the point of delivery. Once customers have taken delivery of their order and signed confirming this, they take ownership of the items and capfieldsevolve.com will be unable to accept any returns.  The following are reasons that enables returns;						
+                  
+      1. DEFECTIVE GOODS AND DISSATISFACTION						
+                  
+      · We guarantee the quality of our goods. You must inspect the goods and notify us immediately by phoning our customer care centre number (07035601099 ) or sending a message.						
+                  
+      · In the event that the Goods collected by you are damaged, defective, faulty or do not match their description at the point of collection, you would be sent a replacement or a full refund will be made to you. You are advised to check your goods for any defect, fault or damage before confirming your delivery. If you discover a genuine fault, defect or damage after delivery, you will be required to return the Goods to us (together with all relevant packaging) immediately.						
+                  
+      2. RETURNS AND RETURNS APPROVAL						
+                  
+      You can return your product as long as:						
+                  
+      · You contact Customer Support within 48 hours of delivery. This is because we cannot vouch for the customer's storage methods after delivery has been made.						
+                  
+      · You have proof of purchase (receipt).						
+                  
+      · The goods must be in an ‘as new’ condition and returned in the original, undamaged packaging along with the accessories.						
+                  
+      · The product must not have been used or opened.						
+                  
+      · All sealed items must not be opened otherwise it would not be accepted on return. Capfields will only accept unsealed items in cases where maggots, insects or moulds are found in the item.						
+                  
+      · Return Approval of All returned items is subject to inspection and approval by the authorized representative of Capfields Evolve Online Mart. For a returned item to be approved for return, it must be in an ‘as new’ condition and returned in the original, undamaged packaging along with the accessories received with it. Items that are not returned in the above condition will be sent back to you and payment will not be refunded.						
+                  
+      · Items returned without all accompanying accessories and packaging such as:						
+                  
+      a. Fashion accessories						
+                  
+      b. Personal hygiene items (combs, toothbrushes, hair brushes, toothpaste and other similar products)						
+                  
+      c. Cosmetic products						
+                  
+      d. Any form of underwear						
+                  
+      e. Fragrances						
+                  
+      f. Beauty products						
+                  
+      g. Books						
+                  
+      h. Toys						
+                  
+      Would be replaced with another of the same product, refunded to the account linked to your debit card (if you paid by debit card) or refunded by bank transfer (if you paid by bank deposit or cash only).						
+                  
+      · You will be asked to choose one of the following options:						
+                  
+      a. The delivery will be free if return is due to Incorrect Delivery.Otherwise, you’ll need to pay for the cost of delivering goods back to Capfields Evolve Online Mart. The period of refund will not exceed 10 working days from receipt of returned goods to us and the accompanying documents, if approval of your application for the return of the goods is granted.						
+                  
+      3. CUSTOMER COMPLAINTS						
+                  
+      · Any Customer complaints should be addressed to the Capfields evolve Customer Care Line (07035601099) or sent to help@capfieldsevolve.com						
+      · When returning an item, ensure all seals, tags and accessories are left intact and item is in it's original packaging. If you have created a password on the device you wish to return, please ensure it is removed, otherwise, your return will be invalid.						
+                  
+      Frequenly asked questions on our refund policy;						
+                  
+      1) How long will it take to return an item and get my refund?						
+                  
+      Ans: Refund Timeline:The refund time starts from the day the item you would like to return is picked up by one of our delivery associates to the day the refund process is completed. Refunds are processed immediately, but it may take up to 2 weeks to see the funds in your account, depending on your bank.						
+      Note: If the reason for returning an item cannot be validated, such item will be redelivered to you and a refund will not be processed.						
+                  
+      2) I have requested for returns pick-up from my home, how long will it take?						
+                  
+      Ans: Once we receive your request, we will make two attempts to retrieve the item(s).						
+                  
+      3) What happens if I miss my attempt to retrieve my return?						
+                  
+      Ans:Your return request will be cancelled and you will no longer be able to return that item.						
+                  
+      4) Can I return my item after the stated returns timeline?						
+                  
+      Ans:You will not be able to return Capfields evolve store items after 10 days and other perishable items after 48 hours.						
+                  
+      5) I initiated a return and requested for pickup at my home, but no one came & it has been a while, what should I do?						
+                  
+      Ans:We apologize for this. Please contact our customer service lines.						
+                  
+      6) My return has been collected for a while, but still no news, what should I do?						
+                  
+      Ans:We apologize for this. Please contact our customer service lines.						
+                  
+      7) Capfields Evolve tells me they have done a bank transfer but I have not received the money, what should I do?						
+                  
+      Ans:It takes 2-5 business days for the money to reflect in your account. Please contact our customer service if it you still haven’t received your refund after this time for more details.						
+                  
+      8) How do I track my return status?						
+                  
+      Ans:We will keep you updated by email and SMS about the status of your return.						
+                  
+      9) Do I have to return all products in my order?						
+                  
+      Ans:No, you do not have to return your entire order. You can return your choice of selected items.						
+                  
+      10) Do I have to pay for shipping charges when I return a product?						
+                  
+      Ans:There are no charges for shipping a product back to Capfields Evolve.						
+                  
+      11) Can I Replace or Exchange an item rather than a refund?						
+                  
+      Ans: At the moment, Capfields Evolve does not offer replacement or exchange of an item; however you can still return an item and receive a full refund.						
+                  
+      12) Why was my item redelivered to me instead of a refund?						
+                  
+      Ans:Returned items will be redelivered to you if we cannot verify your reason for returning it. For example, a microwave returned for not powering-on will be tested extensively and if the claims are right, a refund will be processed, but if during the test, it powers-on, the item will be redelivered to you.						
+                  
+      13) If my return gets validated, how and when will I get my refund?						
+                  
+      Ans:Refund process begins after we have completed the evaluation of your returned product. See information about our refund timelines above. We will send you an email notification once return examination is completed.To process your refund quickly, please note that all refunds will be processed into your Capfields evolve wallet.						
+                  
+      14) I have received my refund: how do I use my refund for future purchases?						
+                  
+      Ans:Your wallet refund can be used to purchase products on Capfields Evolve platform.						
+                  
+      15) If my returned product is not validated for refund, how do I get informed?						
+                  
+      Ans:If your return is invalid then we will call you to explain the issue and send the item(s) back to you. We will arrange the delivery of the item.						
+                  
+      16) Do I also get refunded the shipping charge when returning a product?						
+                  
+      Ans:Yes, the refunded amount will also include the shipping amount you were charged.						
+                  
+      17)How to return an item						
+                  
+      Go to your dashboard and click on RETURN PRODUCT, select the product directly from your shopping history and click on RETURN. you will be directed to fill a form giving reason for return, payment receipt and details of the driver who delivered the product to you.'''),
+          ],
+        ),
+      ),
+    );
+  }
+}
